549a816dd95f05aaac31a3852b30010cf7440f06
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-04-06 18:22:01 +0300

    Максимальное из трех принятых на вход чисел.

---
+++ Homework04/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Clear();
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+    if (max < c)
+    {
+        max = c;
+    }
+}
+Console.WriteLine($"{a}; {b}; {c}; -> {max}");
