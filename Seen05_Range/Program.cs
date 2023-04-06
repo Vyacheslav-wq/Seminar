86fbcdca45f8a1d4df1220a13d59d583a86ae566
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-04-07 02:58:45 +0300

    Решил последние задачи

---
+++ Seen05_Range/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найдите числа от - N до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+for (int n = -a; n < a + 1; n++)
+{
+    Console.Write($"{n} ");
+}
---
+++ Seen07_Last/Program.cs
@@ -0,0 +1,13 @@
+﻿//Трехзначное на вход, на выход последня цифра числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+while(number < 100 || number > 999)
+{
+    Console.WriteLine("Введено неверное число.");
+    Console.Write("Введите *ТРЕХЗНАЧНОЕ* число: ");
+    number = int.Parse(Console.ReadLine());
+}
+int y = number % 10;
+Console.Write("Последняя цифра: .. ");
+Console.Write($"{y}");
