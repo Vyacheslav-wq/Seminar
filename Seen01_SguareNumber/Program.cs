e05656de28ecb06456c0405587ccd4e622ce8fe0
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-04-06 14:09:36 +0300

    Квадрат, не квадрат число от числа.

---
+++ Seen01_SguareNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Квадрат, не квадрат.
+Console.Clear();
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Math.Pow(b, 2) == a}");
+if(Math.Pow(b, 2) == a)
+{
+    Console.WriteLine("a равен b в квадрате. ");
+}else
+{
+    Console.WriteLine("a не равен b в квадрате. ");
+}
